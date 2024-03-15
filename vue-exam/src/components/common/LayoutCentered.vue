a9964bc285695b1590d5eb544bc9410ee76364ee
Kenneth Reyes <reyeskenichi@gmail.com>
2024-03-15 12:34:49 +0800

    Added Layout Center Component

---
+++ vue-exam/src/components/common/LayoutCentered.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="layout-centered"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LayoutCentered",
+};
+</script>
+
+<style lang="scss" scoped>
+.layout-centered {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+}
+
+@media screen and (max-width: 768px) {
+  .layout-centered > div {
+    padding: 20px;
+  }
+}
+</style>
